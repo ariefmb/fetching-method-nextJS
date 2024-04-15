21430768f4c05994ba3a40b7ea55af535b99e3a8
Arief Budiman <mariefbudiman12@gmail.com>
2024-04-15 13:03:56 +0700

    static site generation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,14 @@ export default function Home() {
   return (
     <>
       <Layout pageTitle="Home Page">
-        <Text text="Welcome Ariefmb" />
-        <Image
-          src={profileImg}
-          alt="profile image"
-          priority={true}
-          className="absolute -left-16 bottom-32"
-        />
+        <div className="container flex flex-wrap flex-col items-center">
+          <Text text="Welcome Ariefmb" />
+          <Image
+            src={profileImg}
+            alt="profile image"
+            priority={true}
+          />
+        </div>
       </Layout>
     </>
   );
--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,12 +1,30 @@
 import Layout from "@/components/Layout";
 import Text from "@/components/Text";
+import styles from '@/components/Text/Text.module.css'
 
-const Users = () => {
+async function getUsersData() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const dataUsers = await res.json();
+
+  return dataUsers;
+}
+
+export default async function Users() {
+  const dataUsers = await getUsersData();
+
   return (
-    <Layout  pageTitle="Users Page">
+    <Layout pageTitle="Users Page">
       <Text text="Users Page" />
+      <div className="users-list flex flex-col text-center my-16">
+        <h1 className={`text-3xl font-bold ${styles.container}`}>List of Users :</h1>
+        <ul className={`text-xl ${styles.container}`}>
+          {dataUsers.map((user: any) => (
+            <>
+              <li>{user.name}</li>
+            </>
+          ))}
+        </ul>
+      </div>
     </Layout>
   );
-};
-
-export default Users;
+}
